e27f0440c62137955841b1d3d2a44c51972fd73a
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2020-04-27 14:01:43 +0530

    Add files via upload

---
+++ COVID_INDIA_MAP/maps.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+covid_India_cases = pd.read_csv('covid_19_india.csv')
+covid_India_cases.rename(columns={'State/UnionTerritory': 'State', 'Cured': 'Recovered', 'Confirmed': 'Confirmed'}, inplace=True)
+
+statewise_cases = pd.DataFrame(covid_India_cases.groupby(['State'])['Confirmed', 'Deaths', 'Recovered'].max().reset_index())
+last=statewise_cases
+pos=pd.read_csv('UTM ZONES of INDIA.csv')
+ind_grp=last.merge(pos , left_on='State', right_on='State / Union Territory')
+
+import folium
+map = folium.Map(location=[20.5937, 78.9629], zoom_start=5,tiles='cartodbpositron')
+tooltip="Click to See Data"
+
+for lat, lon,state,Confirmed,Recovered,Deaths in zip(ind_grp['Latitude'], ind_grp['Longitude'],ind_grp['State'],ind_grp['Confirmed'],ind_grp['Recovered'],ind_grp['Deaths']):
+    folium.Marker([lat, lon],
+                        radius=5,
+                        color='blue',
+                      popup =(
+                    'State: ' + str(state) + '<br>'
+                    'Confirmed: ' + str(Confirmed) + '<br>'
+                      'Recovered: ' + str(Recovered) + '<br>'
+                      'Deaths: ' + str(Deaths) + '<br>'),
+                        tooltip=tooltip,
+
+                        fill_color='red',
+                        fill_opacity=0.7 ).add_to(map)
+map
+
+map.save('map.html')
+
+
